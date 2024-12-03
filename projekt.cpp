f8af83b0f445f9508d35811b338d2f5b58d165aa
W1SL4 <konzaw150798@gmail.com>
2024-12-03 20:54:42 +0100

    wysylam projekt

---
+++ projekt.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int liczba;
+	cout << "Podaj liczbe calkowita: ";
+	cin >> liczba;
+        cout << "Podana liczba: " << liczba << endl;
+    return 0;
+}
+
