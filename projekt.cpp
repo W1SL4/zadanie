61d397e01e1875ef37d6d9a9868c076d172a7089
W1SL4 <konzaw150798@gmail.com>
2024-12-03 21:12:23 +0100

    poprawa

--- projekt.cpp
+++ projekt.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int liczba;
-	cout << "Podaj liczbe calkowita: ";
-	cin >> liczba;
-        cout << "Podana liczba: " << liczba << endl;
-    return 0;
+bool czy_pierwsza(int liczba) {
+	    if (liczba < 2) return false;
+	        for (int i = 2; i * i <= liczba; ++i) {
+			        if (liczba % i == 0) return false;
+				    }
+		    return true;
 }
 
+int main() {
+	    int liczba;
+	        cout << "Podaj liczbe calkowita: ";
+		    cin >> liczba;
+		        cout << "Podana liczba: " << liczba;
+			    if (czy_pierwsza(liczba)) {
+				            cout << " - liczba pierwsza" << endl;
+					        } else {
+							        cout << " - nie jest liczba pierwsza" << endl;
+								    }
+			        return 0}
